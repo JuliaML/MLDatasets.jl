90214908395389120f5ffcd0e05a1c1ef722e9ec
Christof Stocker <stocker.christof@gmail.com>
2018-08-26 09:00:23 +0200

    tweak optional dependencies

--- src/MLDatasets.jl
+++ src/MLDatasets.jl
@@ -12,28 +12,28 @@ bytes_to_type(::Type{T}, A::Array{UInt8}) where T<:Number  = convert(Array{T}, r
 global __images_supported__ = false
 global __matfiles_supported__ = false
 
-function _channelview(image::AbstractArray{<:Color})
+function _channelview(image::AbstractArray{<:Colorant})
     __images_supported__ ||
-        error("Converting to/from image requires ImageCore package.")
-    channelview(image)
+        error("Converting to/from image requires `using ImageCore`")
+    ImageCore.channelview(image)
 end
 
 function _colorview(::Type{T}, array::AbstractArray{<:Number}) where T <: Color
     __images_supported__ ||
-        error("Converting to image requires ImageCore package.")
-    colorview(T, array)
+        error("Converting to image requires `using ImageCore`")
+    ImageCore.colorview(T, array)
 end
 
 function _matopen(f::Function, path::AbstractString)
     __matfiles_supported__ ||
-        error("Reading .mat files requires MAT package.")
-    matopen(f, path)
+        error("Reading .mat files requires `using MAT`")
+    MAT.matopen(f, path)
 end
 
 function _matread(path::AbstractString)
     __matfiles_supported__ ||
-        error("Reading .mat files requires MAT package.")
-    matread(path)
+        error("Reading .mat files requires `using MAT`")
+    MAT.matread(path)
 end
 
 
@@ -51,11 +51,9 @@ include("UD_English/UD_English.jl")
 function __init__()
     # initialize optional dependencies
     @require ImageCore="a09fc81d-aa75-5fe9-8630-4744c3626534" begin
-        using ImageCore
         global __images_supported__ = true
     end
     @require MAT="23992714-dd62-5051-b70f-ba57cb901cac" begin
-        using MAT
         global __matfiles_supported__ = true
     end
 end
