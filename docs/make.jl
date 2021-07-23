6d8200a847100e80a99d1038d600b5121efcc095
Carlo Lucibello <carlo.lucibello@gmail.com>
2021-07-23 03:13:39 +0200

    slimmer docs

--- docs/make.jl
+++ docs/make.jl
@@ -1,29 +1,46 @@
 using Documenter, MLDatasets
 
+DocMeta.setdocmeta!(MLDatasets, :DocTestSetup, :(using MLDatasets); recursive=true)
+
+# Build documentation.
+# ====================
+
 makedocs(
     modules = [MLDatasets],
+    doctest = true,
     clean = false,
-    format = Documenter.HTML(
-                prettyurls = haskey(ENV, "CI"), 
-                assets = [joinpath("assets", "favicon.ico")]
-            ),
     sitename = "MLDatasets.jl",
+    format = Documenter.HTML(
+        canonical = "https://juliadata.github.io/MLDatasets.jl/stable/",
+        assets = ["assets/favicon.ico"],
+        prettyurls = get(ENV, "CI", nothing) == "true"
+    ),
+
     authors = "Hiroyuki Shindo, Christof Stocker",
-    linkcheck = !("skiplinks" in ARGS),
     pages = Any[
         "Home" => "index.md",
         "Available Datasets" => Any[
-            "Image Classification" => Any[
-                "MNIST handwritten digits" => "datasets/MNIST.md",
-                "Fashion MNIST" => "datasets/FashionMNIST.md",
+            "Vision" => Any[
+                "MNIST" => "datasets/MNIST.md",
+                "FashionMNIST" => "datasets/FashionMNIST.md",
                 "CIFAR-10" => "datasets/CIFAR10.md",
                 "CIFAR-100" => "datasets/CIFAR100.md",
                 "SVHN format 2" => "datasets/SVHN2.md",
             ],
+            "Misc." => Any[
+                "Iris" => "datasets/Iris.md",
+                "Boston Housing" => "datasets/BostonHousing.md",
+            ],
+            
+            "Text" => Any[
+                "PTBLM" => "datasets/PTBLM.md",
+                "UD_English" => "datasets/UD_English.md",
+            ],
+
         ],
-        hide("Indices" => "indices.md"),
         "LICENSE.md",
     ]
 )
+
 
 deploydocs(repo = "github.com/JuliaML/MLDatasets.jl.git")
--- src/BostonHousing/BostonHousing.jl
+++ src/BostonHousing/BostonHousing.jl
@@ -17,26 +17,27 @@ Number of Attributes: 13 continuous attributes (including target
                             attribute "MEDV"), 1 binary-valued attribute.
                             
 Features:
-       1. CRIM      per capita crime rate by town
-       2. ZN        proportion of residential land zoned for lots over 25,000 sq.ft.
-       3. INDUS     proportion of non-retail business acres per town
-       4. CHAS      Charles River dummy variable (= 1 if tract bounds river; 0 otherwise)
-       5. NOX       nitric oxides concentration (parts per 10 million)
-       6. RM        average number of rooms per dwelling
-       7. AGE       proportion of owner-occupied units built prior to 1940
-       8. DIS       weighted distances to five Boston employment centres
-       9. RAD       index of accessibility to radial highways
-       10. TAX      full-value property-tax rate per 10,000 dollars
-       11. PTRATIO  pupil-teacher ratio by town
-       12. B        1000(Bk - 0.63)^2 where Bk is the proportion of blacks by town
-       13. LSTAT    % lower status of the population
+
+1. CRIM      per capita crime rate by town
+2. ZN        proportion of residential land zoned for lots over 25,000 sq.ft.
+3. INDUS     proportion of non-retail business acres per town
+4. CHAS      Charles River dummy variable (= 1 if tract bounds river; 0 otherwise)
+5. NOX       nitric oxides concentration (parts per 10 million)
+6. RM        average number of rooms per dwelling
+7. AGE       proportion of owner-occupied units built prior to 1940
+8. DIS       weighted distances to five Boston employment centres
+9. RAD       index of accessibility to radial highways
+10. TAX      full-value property-tax rate per 10,000 dollars
+11. PTRATIO  pupil-teacher ratio by town
+12. B        1000(Bk - 0.63)^2 where Bk is the proportion of blacks by town
+13. LSTAT    % lower status of the population
        
 Target:
-       14. MEDV     Median value of owner-occupied homes in 1000's of dollars   
+14. MEDV     Median value of owner-occupied homes in 1000's of dollars   
 
-Note: Variable #14 seems to be censored at 50.00 (corresponding to a median price of \$50,000); 
-Censoring is suggested by the fact that the highest median price of exactly \$50,000 is reported in 16 cases, 
-while 15 cases have prices between \$40,000 and \$50,000, with prices rounded to the nearest hundred. 
+Note: Variable #14 seems to be censored at 50.00 (corresponding to a median price of \\\$50,000); 
+Censoring is suggested by the fact that the highest median price of exactly \\\$50,000 is reported in 16 cases, 
+while 15 cases have prices between \\\$40,000 and \\\$50,000, with prices rounded to the nearest hundred. 
 Harrison and Rubinfeld do not mention any censoring.
 
 The data file stored in this repo is a copy of the This is a copy of UCI ML housing dataset. 
@@ -46,7 +47,7 @@ https://archive.ics.uci.edu/ml/machine-learning-databases/housing/
 
 - [`BostonHousing.features`](@ref)
 - [`BostonHousing.targets`](@ref)
-- [`feature_names`](@ref)
+- [`BostonHousing.feature_names`](@ref)
 """
 module BostonHousing
 
@@ -66,10 +67,10 @@ a 506 element array listing the targets for each example.
 ```jldoctest
 julia> using MLDatasets: BostonHousing
 
-julia> target = BostonHousing.targets()
+julia> target = BostonHousing.targets();
 
 julia> summary(target)
-1x506 Array{Float64,2}
+"1×506 Matrix{Float64}"
 
 julia> target[1]
 24.0
@@ -100,10 +101,10 @@ It has 506 examples.
 ```jldoctest
 julia> using MLDatasets: BostonHousing
 
-julia> features = BostonHousing.features()
+julia> features = BostonHousing.features();
 
 julia> summary(features)
-13x506 Array{Float64,2}
+"13×506 Matrix{Float64}"
 ```
 """
 function features()
