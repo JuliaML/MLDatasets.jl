e930419b0330e7af201c62e1e56f65f3ff88968c
Carlo Lucibello <carlo.lucibello@gmail.com>
2021-07-23 08:46:10 +0200

    little cleanup

--- src/BostonHousing/BostonHousing.jl
+++ src/BostonHousing/BostonHousing.jl
@@ -18,22 +18,23 @@ Number of Attributes: 13 continuous attributes (including target
                             
 Features:
 
-1. CRIM      per capita crime rate by town
-2. ZN        proportion of residential land zoned for lots over 25,000 sq.ft.
-3. INDUS     proportion of non-retail business acres per town
-4. CHAS      Charles River dummy variable (= 1 if tract bounds river; 0 otherwise)
-5. NOX       nitric oxides concentration (parts per 10 million)
-6. RM        average number of rooms per dwelling
-7. AGE       proportion of owner-occupied units built prior to 1940
-8. DIS       weighted distances to five Boston employment centres
-9. RAD       index of accessibility to radial highways
-10. TAX      full-value property-tax rate per 10,000 dollars
-11. PTRATIO  pupil-teacher ratio by town
-12. B        1000(Bk - 0.63)^2 where Bk is the proportion of blacks by town
-13. LSTAT    % lower status of the population
-       
+    1. CRIM      per capita crime rate by town
+    2. ZN        proportion of residential land zoned for lots over 25,000 sq.ft.
+    3. INDUS     proportion of non-retail business acres per town
+    4. CHAS      Charles River dummy variable (= 1 if tract bounds river; 0 otherwise)
+    5. NOX       nitric oxides concentration (parts per 10 million)
+    6. RM        average number of rooms per dwelling
+    7. AGE       proportion of owner-occupied units built prior to 1940
+    8. DIS       weighted distances to five Boston employment centres
+    9. RAD       index of accessibility to radial highways
+    10. TAX      full-value property-tax rate per 10,000 dollars
+    11. PTRATIO  pupil-teacher ratio by town
+    12. B        1000(Bk - 0.63)^2 where Bk is the proportion of blacks by town
+    13. LSTAT    % lower status of the population
+        
 Target:
-14. MEDV     Median value of owner-occupied homes in 1000's of dollars   
+    
+    14. MEDV     Median value of owner-occupied homes in 1000's of dollars   
 
 Note: Variable #14 seems to be censored at 50.00 (corresponding to a median price of \\\$50,000); 
 Censoring is suggested by the fact that the highest median price of exactly \\\$50,000 is reported in 16 cases, 
