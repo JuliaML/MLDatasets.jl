658ee947916351d8e955449f931cd26cea120388
Aman <aman.sharma2020b@vitstudent.ac.in>
2022-02-01 01:49:15 +0530

    titanic.jl added

--- src/Titanic/Titanic.jl
+++ src/Titanic/Titanic.jl
@@ -3,6 +3,9 @@ export Titanic
 module Titanic
 using DataDeps
 using DelimitedFiles
+"""
+The titanic.csv file contains data for 887 of the real Titanic passengers. Each row represents one person. The columns describe different attributes about the person including whether they survived (S), their age (A), their passenger-class (C), their sex (G) and the fare they paid (X).
+"""
 
 export features, targets, feature_names
 const DATA = joinpath(@__DIR__, "titanic.csv")
