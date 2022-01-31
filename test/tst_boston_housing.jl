9fe5715bdf444af36bb1c8f01023ca1c6ff69d78
Aman <aman.sharma2020b@vitstudent.ac.in>
2022-02-01 01:59:18 +0530

    titanic ddataset tests added

--- test/tst_boston_housing.jl
+++ test/tst_boston_housing.jl
@@ -10,9 +10,9 @@ using MLDatasets
     names = Titanic.feature_names()
     @test X isa Matrix{Float64}
     @test Y isa Matrix{Float64}
-    # @test names == ["crim","zn","indus","chas","nox","rm","age","dis","rad","tax","ptratio","b","lstat"]
-    # @test size(X) == (13, 506)
-    # @test size(Y) == (1, 506)
+    @test names == ["crim","zn","indus","chas","nox","rm","age","dis","rad","tax","ptratio","b","lstat"]
+    @test size(X) == (13, 506)
+    @test size(Y) == (1, 506)
 end
 
 end #module
