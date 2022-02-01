63edb3de25a961c2fd9e0240b2ea0a7bc234a1a3
Aman <aman.sharma2020b@vitstudent.ac.in>
2022-02-02 00:28:58 +0530

    final push

--- test/tst_boston_housing.jl
+++ test/tst_boston_housing.jl
@@ -10,7 +10,7 @@ using MLDatasets
     names = BostonHousing.feature_names()
     @test X isa Matrix{Float64}
     @test Y isa Matrix{Float64}
-    @test names == ["crim", "zn", "indus", "chas", "nox", "rm", "age", "dis", "rad", "tax", "ptratio", "b", "lstat"]
+    @test names == ["crim","zn","indus","chas","nox","rm","age","dis","rad","tax","ptratio","b","lstat"]
     @test size(X) == (13, 506)
     @test size(Y) == (1, 506)
 end
