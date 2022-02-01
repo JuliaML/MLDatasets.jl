4bd2bea0af30ef0f502f1d1bf363e848631bf8b0
Aman <aman.sharma2020b@vitstudent.ac.in>
2022-02-01 22:54:49 +0530

    features function added

--- test/tst_boston_housing.jl
+++ test/tst_boston_housing.jl
@@ -5,12 +5,12 @@ using MLDatasets
 
 
 @testset "Boston Housing" begin
-    X  = BostonHousing.features()
-    Y  = BostonHousing.targets()
+    X = BostonHousing.features()
+    Y = BostonHousing.targets()
     names = BostonHousing.feature_names()
     @test X isa Matrix{Float64}
     @test Y isa Matrix{Float64}
-    @test names == ["crim","zn","indus","chas","nox","rm","age","dis","rad","tax","ptratio","b","lstat"]
+    @test names == ["x1", "Pclass", "Name", "Sex", "Age", "SibSp", "Parch", "Ticket", "Fare", "Cabin", "Embarked"]
     @test size(X) == (13, 506)
     @test size(Y) == (1, 506)
 end
