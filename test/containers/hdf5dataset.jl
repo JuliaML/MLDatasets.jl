7bba084edf1fe321f346e39c32fc9fb492ad1481
Kyle Daruwalla <daruwalla@wisc.edu>
2022-02-20 12:55:54 -0600

    Fix broken HDF5 string tests

--- test/containers/hdf5dataset.jl
+++ test/containers/hdf5dataset.jl
@@ -2,9 +2,8 @@ function setup_hdf5dataset_test()
     datasets = [
         ("d1", rand(2, 10)),
         ("g1/d1", rand(10)),
-        # these are broken at HDF5 level
-        # ("g1/d2", string.('a':'j')),
-        # ("g2/g1/d1", "test")
+        ("g1/d2", string.('a':'j')),
+        ("g2/g1/d1", "test")
     ]
 
     fid = h5open("test.h5", "w")
@@ -20,7 +19,7 @@ end
     paths, datas = setup_hdf5dataset_test()
     dataset = HDF5Dataset("test.h5", paths)
     for i in 1:10
-        data = Tuple(map(x -> getobs(x, i), datas))
+        data = Tuple(map(x -> (x isa String) ? x : getobs(x, i), datas))
         @test getobs(dataset, i) == data
     end
     @test numobs(dataset) == 10
