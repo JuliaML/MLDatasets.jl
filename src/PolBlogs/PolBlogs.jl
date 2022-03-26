ea383a0432dedbc87a79dd67a7faa700e8b5a444
Soham Chitnis <sohamchitnis10@gmail.com>
2022-03-26 23:03:31 +0530

    add docstrings and tests

--- src/PolBlogs/PolBlogs.jl
+++ src/PolBlogs/PolBlogs.jl
@@ -2,6 +2,15 @@ export PolBlogs
 
 
 """
+The Political Blogs dataset from the `"The Political Blogosphere and
+    the 2004 US Election: Divided they Blog"
+    <https://dl.acm.org/doi/10.1145/1134271.1134277>`_ paper.
+    `Polblogs` is a graph with 1,490 vertices (representing political
+    blogs) and 19,025 edges (links between blogs).
+    The links are automatically extracted from a crawl of the front page of the
+    blog.
+    Each vertex receives a label indicating the political leaning of the blog:
+    liberal or conservative.
 """
 module PolBlogs
 
@@ -15,9 +24,19 @@ const LINK = "https://netset.telecom-paris.fr/datasets/polblogs.tar.gz"
 const DEPNAME = "PolBlogs"
 const DATA = ["adjaceny.csv","lables.csv"]
 
+"""
+download([dir]; [i_accept_the_terms_of_use])
+Trigger the (interactive) download of the full dataset into
+"`dir`". If no `dir` is provided the dataset will be
+downloaded into "~/.julia/datadeps/$DEPNAME".
+This function will display an interactive dialog unless
+either the keyword parameter `i_accept_the_terms_of_use` or
+the environment variable `DATADEPS_ALWAY_ACCEPT` is set to
+`true`. Note that using the data responsibly and respecting
+copyright/terms-of-use remains your responsibility.
+"""
 download(args...;kw...) = download_dep(DEPNAME,args...;kw...)
-"""
-"""
+
 function __init__()
     register(DataDep(DEPNAME,
     """
@@ -29,6 +48,9 @@ function __init__()
     ))
 end
 """
+    adjacency(; dir = nothing)
+
+Returns a 19025x2 matrix containing edge indices
 """
 function adjacency(;dir=nothing)
     path = datafile(DEPNAME,DATA[0],dir)
@@ -36,10 +58,13 @@ function adjacency(;dir=nothing)
     Matrix{Int64}(adj)
 end
 """
+    lables(; dir = nothing)
+    
+Returns a vector of lables of size 1490
 """
 function lables(;dir=nothing)
     path = datafile(DEPNAME,DATA[1],dir)
     lables = readdlm(path,',')
-    Matrix{Int64}(lables)
+    Vector{Int64}(lables)
 end
 end #module
--- test/tst_polblogs.jl
+++ test/tst_polblogs.jl
@@ -5,6 +5,8 @@ end
 @testset "PolBlogs" begin
     adj = PolBlogs.adjacency()
     lables = PolBlogs.lables()
+    @test adj isa Matrix{Int64}
+    @test lables isa Vector{Int64}
     @test size(adj) == (19025,2)
     @test size(lables) == (1490,)
 end
