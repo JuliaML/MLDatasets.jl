cc2c08726f8cf594bebc62c60ec9f4a38f0e2303
Soham Chitnis <sohamchitnis10@gmail.com>
2022-03-27 10:50:30 +0530

    add docstrings

--- src/PolBlogs/PolBlogs.jl
+++ src/PolBlogs/PolBlogs.jl
@@ -3,14 +3,18 @@ export PolBlogs
 
 """
 The Political Blogs dataset from the `"The Political Blogosphere and
-    the 2004 US Election: Divided they Blog"
-    <https://dl.acm.org/doi/10.1145/1134271.1134277>`_ paper.
-    `Polblogs` is a graph with 1,490 vertices (representing political
-    blogs) and 19,025 edges (links between blogs).
-    The links are automatically extracted from a crawl of the front page of the
-    blog.
-    Each vertex receives a label indicating the political leaning of the blog:
-    liberal or conservative.
+the 2004 US Election: Divided they Blog" <https://dl.acm.org/doi/10.1145/1134271.1134277>`_ paper.
+
+`Polblogs` is a graph with 1,490 vertices (representing political blogs) and 19,025 edges (links between blogs).
+
+The links are automatically extracted from a crawl of the front page of the blog. 
+
+Each vertex receives a label indicating the political leaning of the blog: liberal or conservative.
+# Interface
+- [`PolBlogs.adjacency`](@ref)
+ - [`PolBlogs.labels`](@ref)
+# Utilities
+- [`PolBlogs.download`](@ref)
 """
 module PolBlogs
 
@@ -51,6 +55,11 @@ end
     adjacency(; dir = nothing)
 
 Returns a 19025x2 matrix containing edge indices
+
+```juliarepl
+using MLDatasets : PolBlogs
+adj = PolBlogs.adjacency()
+```
 """
 function adjacency(;dir=nothing)
     path = datafile(DEPNAME,DATA[0],dir)
@@ -59,8 +68,12 @@ function adjacency(;dir=nothing)
 end
 """
     lables(; dir = nothing)
-    
+
 Returns a vector of lables of size 1490
+```juliarepl
+using MLDatasets : PolBlogs
+lables = Polblogs.lables()
+```
 """
 function lables(;dir=nothing)
     path = datafile(DEPNAME,DATA[1],dir)
