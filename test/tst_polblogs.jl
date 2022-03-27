9e14c7b28b4d86bf0f9058d52d546a8dd233c0de
Soham Chitnis <sohamchitnis10@gmail.com>
2022-03-27 11:55:47 +0530

    updated type

--- test/tst_polblogs.jl
+++ test/tst_polblogs.jl
@@ -6,7 +6,7 @@ end
     adj = PolBlogs.adjacency()
     lables = PolBlogs.lables()
     @test adj isa Matrix{Int64}
-    @test lables isa Vector{Int64}
+    @test lables isa Matrix{Int64}
     @test size(adj) == (19025,2)
     @test size(lables) == (1490,1)
 end
