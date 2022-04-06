5b48b15d5f5a88cd803999c9144feb6be55322f2
Carlo Lucibello <carlo.lucibello@gmail.com>
2022-04-06 07:46:21 +0200

    test iris names

--- test/datasets_misc/iris.jl
+++ test/datasets_misc/iris.jl
@@ -2,13 +2,18 @@ n_obs = 150
 n_features = 4
 n_targets = 1
 
+feature_names = ["sepallength", "sepalwidth", "petallength", "petalwidth"]
+target_names = ["class"]
+
 d = Iris()
 test_inmemory_supervised_table_dataset(d;
-    n_obs, n_features, n_targets)
+    n_obs, n_features, n_targets,
+    feature_names, target_names)
 
 d = Iris(as_df=false)
 test_inmemory_supervised_table_dataset(d;
-    n_obs, n_features, n_targets, 
+    n_obs, n_features, n_targets,
+    feature_names, target_names,
     Tx=Float64, Ty=AbstractString)
 
 
