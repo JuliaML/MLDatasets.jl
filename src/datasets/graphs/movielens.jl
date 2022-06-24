bec5d631f1f97be5d578c86b17a71b0beb2c0fbe
Deeptendu <deeptendu.santra@protonmail.com>
2022-06-24 23:59:27 +0530

    MovieLens 100k modular with test

--- src/datasets/graphs/movielens.jl
+++ src/datasets/graphs/movielens.jl
@@ -11,63 +11,116 @@ function __init__movielens()
     ))
 end
 
+"""
+    MovieLens(name; dir=nothing)
+"""
 struct MovieLens
+  name::String
   metadata::Dict{String, Any}
   graphs::Vector{HeteroGraph}
 end
 
 function MovieLens(name::String; dir=nothing)
+    name = lowercase(name)
     create_default_dir("MovieLens")
     dir = movielens_datadir(name, dir)
-    ratings_data_file = "u.data"
+    # format varied in older generations of MovieLens datasets
+    if name == "100k"
+      user_data, movie_data, rating_data = read_100k_data(dir)
+      g = generate_movielens_graph(user_data, movie_data, rating_data)
+      metadata = get_movielens_metadata([user_data, movie_data, rating_data])
+      return MovieLens(name, metadata, [g])
+    else
+      error("Functionality for ml-$name has not been implemented yet")
+    end
+end
+
+function read_100k_data(dir::String)
+  user_data = read_100k_user_data(dir)
+  movie_data = read_100k_movie_data(dir)
+  rating_data = read_100k_rating_data(dir)
+  return (user_data, movie_data, rating_data)
+end
+
+function read_100k_user_data(dir::String)::Dict
+  user_data_file = "u.user"
+  user_data = Dict()
+  user_df = read_csv_asdf(joinpath(dir, user_data_file), header=false)
+
+  user_ids = user_df[!, 1]
+  @assert minimum(user_ids) == 1
+  user_data["num_users"] = maximum(user_ids)
+  user_data["age"] = user_df[!, 2]
+  user_data["gender"] = user_df[!, 3] .== "M" # I hope I don't get cancelled for binarizing this field
+  user_data["occupation"] = user_df[!, 4]
+  user_data["zipcode"] = user_df[!, 5]
+  return user_data
+end
+
+function read_100k_rating_data(dir::String)::Dict
+    rating_data_file = "u.data"
+    rating_info = read_csv(joinpath(dir, rating_data_file), header=false)
+    @assert size(rating_info)[2] == 4
+
+    rating_data = Dict()
+    rating_data["user_movie"] = rating_info[:, 1:2]
+    rating_data["rating"] = rating_info[:, 3]
+    rating_data["timestamp"] = rating_info[:, 4]
+    return rating_data
+end
+
+function read_100k_movie_data(dir::String)::Dict
     movie_data_file = "u.item"
-    user_data_file = "u.user"
-
-    metadata = Dict()
-    node_data = Dict()
-
-    # movie information
     movie_df = read_csv_asdf(joinpath(dir, movie_data_file), header=false, dateformat="dd-u-yyyy")
-    movie_ids = movie_df[!, 1]
-    metadata["movie_names"] = movie_df[!, 2]
-    release_date = movie_df[!, 3]
-    movie_genres = movie_df[!, 6:end] |> Matrix{Bool}
-    @assert minimum(movie_ids) == 1
+    movie_data = Dict()
 
-    # information obtained from 
-    metadata["genre_labels"] = [ "Unknown", "Action", "Adventure", "Animation", "Children", "Comedy",
+    movie_ids = movie_df[!, 1]
+    @assert minimum(movie_ids) == 1
+    movie_data["num_movies"] = maximum(movie_ids)
+    movie_data["release_date"] = movie_df[!, 3]
+    genres = movie_df[!, 6:end] |> Matrix{Bool}
+    genres = permutedims(genres, (2, 1))
+    movie_data["genres"] = genres
+
+    movie_data["metadata"] = Dict()
+    movie_data["metadata"]["movie_names"] = movie_df[!, 2]
+    movie_data["metadata"]["genre_labels"] = [ "Unknown", "Action", "Adventure", "Animation", "Children", "Comedy",
         "Crime", "Documentary", "Drama", "Fantasy", "Film-Noir", "Horror", "Musical",
         "Mystery", "Romance", "Sci-Fi", "Thriller", "War", "Western" ]
+    return movie_data
+end
 
-    node_data["movie"] = Dict("genres" => movie_genres, "release_date" => release_date)
+function generate_movielens_graph(user_data::Dict, movie_data::Dict, rating_data::Dict)
+    num_nodes = Dict("user" => user_data["num_users"], "movie" => movie_data["num_movies"])
+    node_data = Dict()
+    node_data["user"] = Dict(Symbol(k) => maybesqueeze(v) for (k, v) in user_data if k ∉ ["num_users", "metadata"])
+    node_data["movie"] = Dict(Symbol(k) => maybesqueeze(v) for (k, v) in movie_data if k ∉ ["num_movies", "metadata"])
 
-    # user
-    user_df = read_csv_asdf(joinpath(dir, user_data_file), header=false)
-    user_ids = user_df[!, 1]
-    user_age = user_df[!, 2]
-    user_gender = user_df[!, 3] .== "M" # I hope I don't get cancelled for binarizing this field
-    user_occupation = user_df[!, 4]
-    user_zipcode = user_df[!, 5]
-    @assert minimum(user_ids) == 1
+    edges = rating_data["user_movie"]
+    user_ids, movie_ids = edges[:, 1], edges[:, 2]
+    edge_indices = Dict(("user", "rating", "movie") => ([user_ids; movie_ids], [movie_ids; user_ids]))
 
-    node_data["user"] = Dict("age" => user_age, "gender" => user_gender, "occupation" => user_occupation, "zipcode" => user_zipcode)
-    num_nodes = Dict("user" => length(user_ids), "movie" => length(movie_ids))
+    edge_data = Dict(("user", "rating", "movie") => Dict(Symbol(k) => maybesqueeze([v;v]) for (k, v) in rating_data if  k ∉ ["user_movie", "metadata"]))
 
-    # rating
-    ratings_data = read_csv(joinpath(dir, ratings_data_file), header=false)
-    @assert size(ratings_data)[2] == 4
-    user_id, movie_id = ratings_data[:, 1], ratings_data[:, 2]
-    # check the user_id and movie_ids maximum and minimum values
-    edge_indices = Dict(("user", "rating", "movie") => ([user_id; movie_id], [movie_id; user_id]))
-    edge_data = Dict(("user", "rating", "movie") => Dict("rating" => ratings_data[:, 3], "timestamp" => ratings_data[:, end]))
+    return HeteroGraph(; num_nodes, edge_indices, node_data, edge_data)
+end
 
-    g = HeteroGraph(;num_nodes, edge_indices, node_data, edge_data)
-    return MovieLens(metadata, [g])
+function get_movielens_metadata(data::Vector)
+    # the recieved data in generally user_data, movie_data and rating_data
+    metadata = Dict()
+    for d in data
+      if haskey(d, "metadata")
+        for (k, v) in d["metadata"]
+            metadata[k] = v
+        end
+      end
+    end
+    return metadata
 end
 
 function movielens_datadir(name, dir = nothing)
     dir = isnothing(dir) ? datadep"MovieLens" : dir
-    dname = "ml-" * lowercase(name)
+    dname = "ml-" * name
     LINK = "https://files.grouplens.org/datasets/movielens/$dname.zip"
     d  = joinpath(dir, dname)
     if !isdir(d)
@@ -80,3 +133,23 @@ function movielens_datadir(name, dir = nothing)
     @assert isdir(d)
     return d
 end
+
+function Base.show(io::IO, ::MIME"text/plain", d::MovieLens)
+    recur_io = IOContext(io, :compact => false)
+
+    print(io, "MovieLens $(d.name):")  # if the type is parameterized don't print the parameters
+
+    for f in fieldnames(MovieLens)
+        if !startswith(string(f), "_") && f != :name
+            fstring = leftalign(string(f), 10)
+            print(recur_io, "\n  $fstring  =>    ")
+            # show(recur_io, MIME"text/plain"(), getfield(d, f))
+            # println(recur_io)
+            print(recur_io, "$(_summary(getfield(d, f)))")
+        end
+    end
+end
+
+Base.length(data::MovieLens) = length(data.graphs)
+Base.getindex(data::MovieLens, ::Colon) = length(data.graphs) == 1 ? data.graphs[1] : data.graphs
+Base.getindex(data::MovieLens, i) = getobs(data.graphs, i)
--- test/datasets/graphs.jl
+++ test/datasets/graphs.jl
@@ -172,3 +172,44 @@ end
         end
     end
 end
+
+@testset "ml-100k" begin
+    data = MovieLens("100k")
+    @test length(data) == 1
+
+    g = data[1]
+    @test g == data[:]
+    @test g isa MLDatasets.HeteroGraph
+
+    num_nodes = Dict(
+        "movie"  => 1682,
+        "user"   => 943,
+        )
+    num_edges = Dict(
+        ("user", "rating", "movie") => 200000
+    )
+
+    for type in keys(num_nodes)
+        @test type ∈ g.node_types
+        @test g.num_nodes[type] == num_nodes[type]
+        node_data = get(g.node_data, type, nothing)
+        @assert !isnothing(node_data)
+        for (key, val) in node_data
+            @assert key ∈ [:release_date, :genres, :age, :occupation, :zipcode, :gender]
+            @test size(val)[end] == num_nodes[type]
+        end
+    end
+
+    for type in keys(num_edges)
+        @test type ∈ g.edge_types
+        @test g.num_edges[type] == num_edges[type]
+        @test length(g.edge_indices[type][1]) == num_edges[type]
+        @test length(g.edge_indices[type][2]) == num_edges[type]
+        edge_data = g.edge_data[type]
+        for (key, val) in edge_data
+            @assert key in  [:timestamp, :rating]
+            @test ndims(val) == 1
+            @test size(val)[end] == num_edges[type]
+        end
+    end
+end
