beaf16dc8897980f76ada5df45309edacf590f83
Deeptendu <deeptendu.santra@protonmail.com>
2022-07-26 16:01:45 -0400

    PTBLM tests

--- test/datasets/text.jl
+++ test/datasets/text.jl
@@ -1,16 +1,17 @@
 
 @testset "PTBLM" begin
-    n_obs = 5574
     n_targets = 1
     n_features = ()
-    Tx=String
-    Ty=String
-    
-    d = SMSSpamCollection()
-    
-    test_supervised_array_dataset(d;
-        n_obs, n_targets, n_features,
-        Tx, Ty)
+    Tx=Vector{String}
+    Ty=Vector{String}
+
+    for (n_obs, split) in [(42068, :train), (3761, :test)] 
+        d = PTBLM(split)
+
+        test_supervised_array_dataset(d;
+            n_obs, n_targets, n_features,
+            Tx, Ty)
+    end
 end
 
 
@@ -33,7 +34,7 @@ end
     Tx = Vector{Vector{String}}
     for (n_obs, split) in [(12543, :train), (2077, :test), (2001, :dev)] 
         d = UD_English(split)
-        
+
         test_unsupervised_array_dataset(d;
             n_obs, n_features, Tx)
     end
