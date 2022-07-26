b6e108736f7ac4659e3abf184fe073c474b3a81a
Deeptendu <deeptendu.santra@protonmail.com>
2022-07-26 15:41:17 -0400

    Remove test for SMS Spam

--- test/datasets/text.jl
+++ test/datasets/text.jl
@@ -13,21 +13,6 @@
         Tx, Ty)
 end
 
-
-@testset "SMSSpamCollection" begin
-    n_obs = 5574
-    n_targets = 1
-    n_features = ()
-    Tx=String
-    Ty=String
-    
-    d = SMSSpamCollection()
-    
-    test_supervised_array_dataset(d;
-        n_obs, n_targets, n_features,
-        Tx, Ty)
-end
-
 @testset "UD_English" begin
     n_features = ()
     Tx = Vector{Vector{String}}
