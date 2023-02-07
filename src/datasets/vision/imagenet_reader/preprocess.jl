6af86c62c8b0a7f0d81cd72792f0bd57658aa8a5
Adrian Hill <adrian.hill@mailbox.org>
2023-02-07 13:34:50 +0100

    Remove normalization constants

--- src/datasets/vision/imagenet_reader/preprocess.jl
+++ src/datasets/vision/imagenet_reader/preprocess.jl
@@ -1,25 +1,14 @@
-# Image preprocessing for ImageNet models.
-# Code adapted from Metalhead 0.5.3's utils.jl
-
-# Coefficients taken from PyTorch's ImageNet normalization code
-const PYTORCH_MEAN = [0.485f0, 0.456f0, 0.406f0]
-const PYTORCH_STD = [0.229f0, 0.224f0, 0.225f0]
-
-normalize_pytorch(x) = (x .- PYTORCH_MEAN) ./ PYTORCH_STD
-inv_normalize_pytorch(x) = x .* PYTORCH_STD .+ PYTORCH_MEAN
+# Image preprocessing defaults for ImageNet models.
 
 function default_preprocess(im::AbstractMatrix{<:AbstractRGB})
-    # Similar to the validation dataset loader in PyTorch's ImageNet example
-    # https://github.com/pytorch/examples/blob/91ccd7a21be6fa687000beef82fc1e5d7d64e4bd/imagenet/main.py#L223-L230
-    im = center_crop(im)
-    im = normalize_pytorch(channelview(im))
+    im = channelview(center_crop(im))
     return PermutedDimsArray(im, (3, 2, 1)) # Convert from Image.jl's CHW to Flux's WHC
 end
 
 function default_inverse_preprocess(x::AbstractArray{T,N}) where {T,N}
     @assert N == 3 || N == 4
     x = PermutedDimsArray(x, (3, 2, 1, 4:N...)) # Convert from WHC[N] to CHW[N]
-    return colorview(RGB, inv_normalize_pytorch(x))
+    return colorview(RGB, x)
 end
 
 # Take rectangle of pixels of shape `outsize` at the center of image `im`
