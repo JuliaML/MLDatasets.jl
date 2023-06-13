307f0092208a8e99b1c4c0c53565d95ee4e84d86
aurorarossi <aurora98rossi@gmail.com>
2023-06-13 16:18:03 +0200

    Add some functions for METR-LA

---
+++ src/datasets/graphs/metrla.jl
@@ -0,0 +1,39 @@
+function __init__metrla()
+    DEPNAME = "METR-LA"
+    LINK = "https://graphmining.ai/temporal_datasets/"
+    register(ManualDataDep(DEPNAME,
+                           """
+                           Dataset: $DEPNAME
+                           Website : $LINK
+                           """,
+                           LINK))
+end
+
+struct METRLA 
+    graphs::Vector{Graph}
+end
+
+function metrla_datadir(dir = nothing)
+    dir = isnothing(dir) ? datadep"METR-LA" : dir
+    dname = "METR-LA"
+    LINK = "https://graphmining.ai/temporal_datasets/$dname.zip"
+    d = joinpath(dir, dname)
+    if !isdir(d)
+        DataDeps.fetch_default(LINK, dir)
+        currdir = pwd()
+        cd(dir) # Needed since `unpack` extracts in working dir
+        DataDeps.unpack(joinpath(dir, "$dname.zip"))
+        # conditions when unzipped folder is our required data dir
+        cd(currdir)
+    end
+    @assert isdir(d)
+    return d
+end
+
+function read_metrla()
+    d = metrla_datadir()
+    adj_matrix = npzread(joinpath(d, "adj_mat.npy"))
+    node_features = npzread(joinpath(d, "node_values.npy"))
+end
+    
+end
