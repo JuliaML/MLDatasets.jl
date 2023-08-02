60b851cb0fbae2a41c51e64ca708c1773ab8b365
Pietro Monticone <pietromonticone.cs@gmail.com>
2023-08-02 10:18:24 -0500

    Update smsspamcollection.jl

--- src/datasets/text/smsspamcollection.jl
+++ src/datasets/text/smsspamcollection.jl
@@ -21,7 +21,7 @@ end
 
 The SMS Spam Collection v.1 (hereafter the corpus) is a set of SMS tagged messages 
 that have been collected for SMS Spam research. It contains one set of SMS messages
-in English of 5,574 messages, tagged acording being ham (legitimate) or spam.
+in English of 5,574 messages, tagged according being ham (legitimate) or spam.
 The corpus has a total of 4,827 SMS legitimate messages (86.6%) and a total of 747 (13.4%) spam messages.
 
 The corpus has been collected by Tiago Agostinho de Almeida (http://www.dt.fee.unicamp.br/~tiago) 
