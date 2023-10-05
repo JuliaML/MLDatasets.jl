6a7940fb5925a25b3a5c5e90511cf785cfcf6898
aurorarossi <aurora98rossi@gmail.com>
2023-10-05 18:07:20 +0200

    Optimized version

--- src/datasets/graphs/temporalbrains.jl
+++ src/datasets/graphs/temporalbrains.jl
@@ -24,32 +24,54 @@ function tb_datadir(dir = nothing)
     return dir
 end
 
-function create_tbdataset(dir)
+
+function create_tbdataset(dir, thre)
     name_filelabels = joinpath(dir, "LabelledTBN", "labels.txt")
     filelabels = open(name_filelabels, "r")
-    temporalgraphs = Vector{MLDatasets.TemporalSnapshotsGrapgs}(undef, 1000)
+    temporalgraphs = Vector{MLDatasets.TemporalSnapshotsGraph}(undef, 1000)
 
     for (i,line) in enumerate(eachline(filelabels))
         id, gender, age = split(line)
-        
         name_network_file = joinpath(dir, "LabelledTBN", "networks", id * "_ws60_wo30_tuk0_pearson_schaefer_100.txt")
         
-        data = readdlm(name_network_file,','; skipstart = 1) 
-        edata = [data[data[:,3].==t,4] for t in 1:27]
+        data = readdlm(name_network_file,',',Float32; skipstart = 1) 
+    
+        data_thre = view(data,view(data,:,4) .> thre,:)
+        data_thre_int = Int.(view(data_thre,:,1:3))
 
-        temporalgraphs[i] = TemporalSnapshotsGraph(num_nodes=ones(27).*102, edge_index = (data[:,1],data[:,2],data[:,3]), edge_data = edata, graph_data= (g = gender, a = age))
+        activation = [zeros(Float32, 102) for _ in 1:27]
+        for t in 1:27
+             for n in 1:102
+                rows = (view(data_thre_int,:,1).==n .&& view(data_thre_int,:,3).==t)
+                activation[t][n] = mean(view(data_thre,rows,4))
+             end
+        end
+
+        temporalgraphs[i] = TemporalSnapshotsGraph(num_nodes=ones(Int,27)*102, edge_index = (data_thre_int[:,1], data_thre_int[:,2], data_thre_int[:,3]), node_data= activation, graph_data= (g = gender, a = age))
     end
     return temporalgraphs
 end
 
+"""
+    TemporalBrains(; dir=nothing, threshold_value = 0.6)
+
+The TemporalBrains dataset contains a collection of temporal brain networks of 1000 subjects obtained from resting-state fMRI data from the Human Connectome Project (HCP).
+
+The number of nodes is fixed for each of the 27 snapshots at 102, instead the edge weights change their values over time.
+The edge weights are contained in a vector of 27 arrays in the `edge_data` field of the `TemporalSnapshotsGraph`. 
+
+Each graph has a label representing their gender ("M" for male and "F" for female) and age range (22-25, 26-30,31-35 and 36+) contained as a named tuple in `graph_data`.
+
+The `threshold_value` is used to binarize the edge weights and is set to 0.6 by default.
+"""
 struct TemporalBrains <: AbstractDataset
-    graphs::Vector{TemporalSnapshotsGraph}
+    graphs::Vector{MLDatasets.TemporalSnapshotsGraph}
 end
 
-function TemporalBrains(; dir=nothing)
+function TemporalBrains(;threshold_value = 0.6, dir=nothing)
     create_default_dir("TemporalBrains")
     dir = tb_datadir(dir)
-    graphs = create_tbdataset(dir)
+    graphs = create_tbdataset(dir, threshold_value)
     return TemporalBrains(graphs)
 end
 
